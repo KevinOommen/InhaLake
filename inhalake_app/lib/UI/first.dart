a3f471829f13f36ec92c49972f60c329a9b0a33e
Kevin Oommen <kevinoommenanish@gmail.com>
2023-03-19 11:33:40 +0530

    Modified First Screen

--- inhalake_app/lib/UI/first.dart
+++ inhalake_app/lib/UI/first.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class first extends StatefulWidget {
@@ -13,59 +14,97 @@ class _firstState extends State<first> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 17, 29, 59),
-      body: Column(
-        children: [
-          Expanded(
-              child: Image.asset(
-            'assets/get-started.png',
-            scale: 2,
-          )),
-          Container(
-            height: 70,
-            width: 350,
-            decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 31, 43, 71),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(100),
-                    topRight: Radius.circular(100),
-                    bottomLeft: Radius.circular(100),
-                    bottomRight: Radius.circular(100))),
-            child: Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 80),
-                  child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                            "/login/", (route) => false);
-                      },
-                      child: const Text(
-                        'login',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 236, 239, 240),
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      )),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                child: Image.asset(
+              'assets/get-started.png',
+              scale: 0.5,
+            )),
+            
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text("Breathe in.\nDrink up.\nMonitor your air and water quality with ease!!!",
+                  style:GoogleFonts.getFont("Poppins",
+                                    textStyle: const TextStyle(
+                                      color: Color.fromARGB(255, 253, 250, 250),
+                                      fontSize: 22,
+                                    )) ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(left: 80),
-                  child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                            "/register/", (route) => false);
-                      },
-                      child: const Text(
-                        'register',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 236, 239, 240),
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      )),
-                ),
-              ],
+              ),
+            )
+                ,
+            Container(
+              height: 70,
+              width: 350,
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 31, 43, 71),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(100),
+                      topRight: Radius.circular(100),
+                      bottomLeft: Radius.circular(100),
+                      bottomRight: Radius.circular(100))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      //set background color of button to white
+                      
+                      style: ButtonStyle(
+                      backgroundColor:  MaterialStateProperty.all(Color(0xFFFFFFFF)),
+                      shape:MaterialStateProperty.all<RoundedRectangleBorder>(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(100),
+                              bottomLeft: Radius.circular(100),
+                              topRight: Radius.circular(100),
+                              bottomRight: Radius.circular(100),),
+                              
+                        ),
+                      )
+                    ),
+                        onPressed: () {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              "/login/", (route) => false);
+                        },
+                        child: const Text(
+                          'login',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 15, 15, 16),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                  const SizedBox(width:10),
+                  Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              "/register/", (route) => false);
+                        },
+                        child: const Text(
+                          'register',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 236, 239, 240),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                  
+                ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+                    height: 20,
+                  )
+          ],
+        ),
       ),
     );
   }
